513f84807990f910c55b04b013d08e4d294b5836
Hotta_Yutaro <g1327061@st.t-kougei.ac.jp>
2016-07-12 16:11:52 +0900

    スポナーの上限設定。無駄なスクリプトの削除

--- Tiny_Breaker/Assets/Resources/Scripts/Spawner.cs
+++ Tiny_Breaker/Assets/Resources/Scripts/Spawner.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour
 {
-    public int spawn_Num = 1;       //生成数
+    public int spawn_Num = 1;       //同時生成数
+    public int maxSpawns = 10;      //生成数の限界
     public GameObject spawan_Obj;   //生成オブジェクト
     public float spawn_Count = 3;   //生成感覚の時間設定
     public float r = 1;             //生成する場所の半径の距離
 
     private float timer;
+
+    private GameObject clone;
 
 	// Use this for initialization
 	void Start ()
@@ -19,20 +22,30 @@ public class Spawner : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+        SpownSoldier();
+    }
+
+    void SpownSoldier()
+    {
         timer += Time.deltaTime;
 
         //生成時間に達しているか確認
-        if(timer > spawn_Count)
+        if (timer > spawn_Count)
         {
             //時間をリセット
             timer = 0;
 
-            //生成数までループ
-            for(int i = 0; i < spawn_Num; i++)
+            //最大生成数の上限かを確認
+            if (this.gameObject.transform.childCount < maxSpawns)
             {
-                Vector3 vec = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f)).normalized * r;
-                Instantiate(spawan_Obj, transform.position + vec, spawan_Obj.transform.rotation);
+                //生成数までループ
+                for (int i = 0; i < spawn_Num; i++)
+                {
+                    Vector3 vec = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f)).normalized * r;
+                    clone = (GameObject)Instantiate(spawan_Obj, transform.position + vec, spawan_Obj.transform.rotation);
+                    clone.transform.parent = this.gameObject.transform;
+                }
             }
         }
-	}
+    }
 }
