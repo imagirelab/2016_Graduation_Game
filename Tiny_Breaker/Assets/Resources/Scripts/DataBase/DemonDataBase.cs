a2079c219052b6b6ac3cb8481627124e2fd871ac
Satoshi Tanaka <yoo3006@gmail.com>
2016-07-24 15:39:17 +0900

    悪魔の体重100にしてDataBaseの親クラスを削除。どのオブジェクトの種類のリストでエラーが出たか知るため。

--- Tiny_Breaker/Assets/Resources/Scripts/DataBase/DemonDataBase.cs
+++ Tiny_Breaker/Assets/Resources/Scripts/DataBase/DemonDataBase.cs
@@ -1,14 +1,51 @@
 ﻿//悪魔のデータを溜めるクラス
 
+using UnityEngine;
+using System.Collections.Generic;
+
 namespace StaticClass
 {
-    public class DemonDataBase : DataBase
+    public class DemonDataBase
     {
         static DemonDataBase dataBase = new DemonDataBase();
         
         public static DemonDataBase getInstance()
         {
             return dataBase;
+        }
+
+        List<GameObject> list = new List<GameObject>();
+
+        public void ClearList()
+        {
+            list.Clear();
+        }
+
+        public void AddList(GameObject item)
+        {
+            list.Add(item);
+        }
+
+        public void RemoveList(GameObject item)
+        {
+            list.Remove(item);
+        }
+
+        //一番近い魂を返す
+        public GameObject GetNearestObject(Vector3 center)
+        {
+            if (list.Count == 0)
+                return null;
+
+            GameObject nearestObject = list[0];
+
+            foreach (var e in list)
+            {
+                if (Vector3.Distance(center, e.gameObject.transform.position) < Vector3.Distance(center, nearestObject.gameObject.transform.position))
+                    nearestObject = e;
+            }
+
+            return nearestObject;
         }
     }
 }
