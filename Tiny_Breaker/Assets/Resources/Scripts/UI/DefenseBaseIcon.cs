7e30ba3e0957c5685b8a65d2314da4e8f496f7b8
satoshi <yoo3006@gmail.com>
2016-10-04 18:05:48 +0900

    制限時間をつけました。

---
+++ Tiny_Breaker/Assets/Resources/Scripts/UI/DefenseBaseIcon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DefenseBaseIcon : MonoBehaviour
+{
+    [SerializeField]
+    DefenseBase player;
+
+    [SerializeField]
+    Sprite iconFine = new Sprite();
+    [SerializeField]
+    Sprite iconPinch = new Sprite();
+
+    [SerializeField]
+    float pinch = 0.25f;
+
+    Image image;
+
+    void Start()
+    {
+        if (player == null)
+            player = new DefenseBase();
+
+        if (GetComponent<Image>())
+            image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        float HPRate = (float)player.HPpro / (float)player.GetHP;
+
+        if(HPRate <= pinch)
+            image.sprite = iconPinch;
+        else
+            image.sprite = iconFine;
+    }
+}
---
+++ Tiny_Breaker/Assets/TimeLimit.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeLimit : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] numbars;
+
+    [SerializeField]
+    Image ones;
+    [SerializeField]
+    Image tens;
+    [SerializeField]
+    Image hundreds;
+
+    [SerializeField]
+    int stateTime = 120;
+    float currentTime = 0.0f;
+
+    bool IsCounting = false;
+
+    void Start ()
+    {
+        if(!IsCounting)
+        {
+            IsCounting = true;
+            currentTime = (float)stateTime;
+        }
+    }
+	
+	void Update ()
+    {
+	    if(IsCounting)
+        {
+            currentTime -= Time.deltaTime;
+
+            if (currentTime <= 0)
+            {
+                currentTime = 0.0f;
+                IsCounting = false;
+            }
+
+            int hundredsNum = (Mathf.FloorToInt(currentTime) % 1000) / 100;
+            int tensNum = (Mathf.FloorToInt(currentTime) % 100) / 10;
+            int onesNum = Mathf.FloorToInt(currentTime) % 10;
+            
+            if (hundredsNum < 10)
+                hundreds.sprite = numbars[hundredsNum];
+            if (tensNum < 10)
+                tens.sprite = numbars[tensNum];
+            if (onesNum < 10)
+                ones.sprite = numbars[onesNum];
+        }
+	}
+}
