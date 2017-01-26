5a04299587bf6bba0960a2d78b430f9ec3448f9e
satoshi <yoo3006@gmail.com>
2017-01-26 14:54:00 +0900

    result画面のジングルを入れました。

---
+++ Tiny_Breaker/Assets/Scripts/Result/ResultJingle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultJingle : MonoBehaviour
+{
+    public AudioSource start;
+    public AudioSource loop;
+
+    bool loopstart = false;
+
+    void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		if(!start.isPlaying && !loopstart)
+        {
+            loopstart = true;
+            loop.Play();
+        }
+	}
+}
